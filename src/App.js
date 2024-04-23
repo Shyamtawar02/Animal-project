6790231525ae2c236013934d7cb9768038420e65
Shyamtawar02 <140075491+Shyamtawar02@users.noreply.github.com>
2024-04-23 11:04:01 +0530

    Added React

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Footer from './Component/Footer';
+import Header from './Component/Header';
+import Home from './Component/Home';
+
+
+
+
+function App() {
+  return <>
+  <Header/>
+  <Home/>
+  <Footer/>
+  </>
+}
+
+export default App;
---
+++ src/Component/Home.js
@@ -0,0 +1,163 @@
+function Home() {
+    return <div className="container-fluid  " style={{ height: "auto" }}>
+
+        <div className="container-fluid" style={{ height: "530px" }}>
+            <img className="border border-secondary" style={{ width: "1310px", height: "520px" }} src="https://img.freepik.com/premium-photo/hyper-realistic-group-cute-dogs_862994-49166.jpg" />
+            <div className="container d-flex justify-content-center text-light" style={{ marginTop: "-200px" }}>
+                <h2>A BETTER LIFE FOR INDIA'S STRAY ANIMALS</h2>
+            </div>
+            <div className="container d-flex justify-content-center text-light mt-4" >
+                <h6>Stray animals roam the streets of India without food, veterinary care or refuge from the element Help us give them a chance</h6>
+            </div>
+            <div className="container d-flex justify-content-center mt-3">
+                <button type="button" class="btn btn-warning  " style={{ backgroundColor: "rgb(246,124,35)", color: "white", fontSize: "14px", border: "none" }}>Donate</button>
+                <button type="button" class="btn btn-warning  ms-4 " style={{ backgroundColor: "rgb(246,124,35)", color: "white", fontSize: "14px", border: "none" }}>Adopt</button>
+            </div>
+        </div>
+
+
+        <div className="container-fluid d-flex justify-content-between" >
+            <div>
+                <img src="https://cdn.cdnparenting.com/articles/2023/09/04181406/2234782329-696x476.jpg" style={{ height: "500px", width: "650px" }} />
+            </div>
+            <div>
+                <div className="d-flex justify-content-center me-3" style={{ marginTop: "150px", color: "black" }}>
+                    <h3 className="fw-bolder">79.9 MILLION ANIMALS AT RISK</h3>
+                </div>
+                <div className="d-flex justify-content-center" style={{ marginTop: "20px" }}>
+                    <p style={{ width: "68%" }}>In India, street dogs are misunderstood as dangerous, savage animals. SAFI works alongside India's local shelters to rescue, rehabilitate, vaccinate, and house these strays in need .</p>
+                </div>
+                <div className="d-flex justify-content-start" style={{ marginTop: "30px", marginLeft: "100px" }}>
+                    <button type="button" class="btn btn-warning   " style={{ backgroundColor: "rgb(246,124,35)", color: "white", fontSize: "14px", border: "none" }}>About Us</button>
+                </div>
+            </div>
+        </div>
+        <div className="container-fluid d-flex justify-content-between mt-3">
+            <div>
+                <div className="d-flex justify-content-center me-3" style={{ marginTop: "150px", color: "black" }}>
+                    <h3 className="fw-bolder" style={{ width: "68%" }}>HOUSING, CARE,& MORE FOR INDIA'S MOST VULNERABLE</h3>
+                </div>
+                <div className="d-flex justify-content-center" style={{ marginTop: "20px" }}>
+                    <p style={{ width: "68%" }}>Since 2020, we've helped build up India's animal welfare infrastructure providing medical care, food, forever home matching, education, and much more. Your tax- deductible donation helps us fight for stray animals in India .</p>
+                </div>
+                <div className="d-flex justify-content-start" style={{ marginTop: "20px", marginLeft: "100px" }}>
+                    <button type="button" class="btn btn-warning   " style={{ backgroundColor: "rgb(246,124,35)", color: "white", fontSize: "14px", border: "none" }}>Donate</button>
+                </div>
+            </div>
+            <div>
+                <img src="https://media.istockphoto.com/id/1316709701/photo/crop-volunteer-stroking-happy-stray-dog.jpg?s=612x612&w=0&k=20&c=3wQkposCjT4AraEr3JPbGFX7jkU4fsWrtV45qLOYOyg=" style={{ height: "450px", width: "660px" }} />
+            </div>
+
+        </div>
+        <div className="conatiner-fluid mt-3" style={{ height: "380px", backgroundColor: "rgb(108,229,154)" }}>
+            <div className="d-flex justify-content-center text-light">
+                <h4 className="mt-3">AT A GLANCE</h4>
+            </div>
+            <div className="d-flex justify-content-evenly text-light mt-5" >
+                <div className="  border border-light rounded-4" style={{ height: "230px", width: "190px", backgroundColor: "rgb(87,201,130)" }}>
+                    <div className="d-flex justify-content-center">
+                        <img src="https://i0.wp.com/strayanimalfoundationindia.org/wp-content/uploads/2022/04/SAFI_1.png?resize=601%2C601&ssl=1" style={{ height: "120px", width: "100px", marginTop: "13px" }} />
+                    </div>
+                    <div className="d-flex justify-content-center">
+                        <h4>1500+</h4>
+                    </div>
+                    <div className="d-flex justify-content-center">
+                        <p style={{ fontSize: "14px" }}>saved animal</p>
+                    </div>
+                </div>
+                <div className=" border border-light rounded-4" style={{ height: "230px", width: "190px", backgroundColor: "rgb(87,201,130)" }}>
+                    <div className="d-flex justify-content-center">
+                        <img src="https://i0.wp.com/strayanimalfoundationindia.org/wp-content/uploads/2022/04/SAFI_2.png?resize=601%2C601&ssl=1" style={{ height: "120px", width: "100px", marginTop: "13px" }} />
+                    </div>
+                    <div className="d-flex justify-content-center">
+                        <h4>950+</h4>
+                    </div>
+                    <div className="d-flex justify-content-center text-center">
+                        <p style={{ fontSize: "14px", width: "60%" }}>animals spayed & neutered</p>
+                    </div>
+                </div>
+                <div className=" border border-light rounded-4" style={{ height: "230px", width: "190px", backgroundColor: "rgb(87,201,130)" }}>
+                    <div className="d-flex justify-content-center">
+                        <img src="https://i0.wp.com/strayanimalfoundationindia.org/wp-content/uploads/2022/04/SAFI_5.png?resize=601%2C601&ssl=1" style={{ height: "120px", width: "100px", marginTop: "13px" }} />
+                    </div>
+                    <div className="d-flex justify-content-center">
+                        <h4>120+</h4>
+                    </div>
+                    <div className="d-flex justify-content-center text-center">
+                        <p style={{ fontSize: "14px", width: "60%" }}>dogs adopted, 310 released</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="container-fluid mt-3 d-flex justify-content-evenly align-items-center" style={{ height: "250px" }}>
+            <div className=" rounded-2" style={{ height: "180px", width: "220px", backgroundColor: "rgb(87,201,130)" }}>
+                <div className="d-flex justify-content-center text-light mt-3 ">
+                    <h5>DONATE</h5>
+                </div>
+                <div className="d-flex justify-content-center text-center text-light">
+                    <p className="text-center" style={{ fontSize: "13px", width: "60%" }}>Every rupees makes difference  the lives  these animals. We offers many ways.</p>
+                </div>
+                <div className="d-flex justify-content-center ">
+                    <button type="button" style={{ fontSize: "10px", color: "white", backgroundColor: "rgb(246,124,35)", border: "none" }}>Learn More</button>
+                </div>
+            </div>
+            <div className="rounded-2" style={{ height: "180px", width: "220px", backgroundColor: "rgb(246,124,35)" }}>
+                <div className="d-flex justify-content-center text-light mt-3 ">
+                    <h5>ADOPT</h5>
+                </div>
+                <div className="d-flex justify-content-center text-center text-light">
+                    <p className="text-center" style={{ fontSize: "13px", width: "60%" }}>Everyone deserves a home. See how you can help our shelter animals find  families .</p>
+                </div>
+                <div className="d-flex justify-content-center ">
+                    <button type="button" style={{ fontSize: "10px", color: "white", backgroundColor: "rgb(87,201,130)", border: "none" }}>Learn More</button>
+                </div>
+            </div>
+            <div className=" rounded-2" style={{ height: "180px", width: "220px", backgroundColor: "rgb(87,201,130)" }}>
+                <div className="d-flex justify-content-center text-light mt-3 ">
+                    <h5>Rescue Animals</h5>
+                </div>
+                <div className="d-flex justify-content-center align-items-center text-center text-light ">
+                    <p class="text-center" style={{ fontSize: "13px", width: "60%" }}>Every creature deserves compassion and care, animals need of a loving home.</p>
+                </div>
+                <div className="d-flex justify-content-center ">
+                    <button type="button" style={{ fontSize: "10px", color: "white", backgroundColor: "rgb(246,124,35)", border: "none" }}>Learn More</button>
+                </div>
+            </div>
+            <div className=" rounded-2" style={{ height: "180px", width: "220px", backgroundColor: "rgb(246,124,35)" }}>
+                <div className="d-flex justify-content-center text-light mt-3 ">
+                    <h5>Doctors</h5>
+                </div>
+                <div className="d-flex justify-content-center align-items-center text-center text-light">
+                    <p className="text-center" style={{ fontSize: "13px", width: "60%" }}>Every life is precious, and our doctors work tirelessly to save and improve them.</p>
+                </div>
+                <div className="d-flex justify-content-center ">
+                    <button type="button" style={{ fontSize: "10px", color: "white", backgroundColor: "rgb(87,201,130)", border: "none" }}>Learn More</button>
+                </div>
+            </div>
+        </div>
+        <div className="container  border border-dark rounded-5 mt-3" style={{ height: "460px" }}>
+            <div className="d-flex justify-content-evenly mt-5">
+                <img src="https://media.istockphoto.com/id/1345007530/photo/young-woman-in-animal-shelter.jpg?s=612x612&w=0&k=20&c=4fkUStW8O3IKvkNw8OyOUsoB8Z-mDkUjR7QFLbuyDig=" style={{ height: "230px", width: "250px" }} />
+                <img src="https://img.freepik.com/free-photo/woman-playing-with-rescue-dogs-shelter_23-2148682984.jpg?size=626&ext=jpg&ga=GA1.1.1700460183.1712275200&semt=ais" style={{ height: "230px", width: "250px" }} />
+                <img src="https://s.yimg.com/ny/api/res/1.2/vNAVDX5yMXzPC9TA.lT.jA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTM2MA--/https://media.zenfs.com/en/first_for_women_876/a28847243a56b582280066897c907b74" style={{ height: "230px", width: "250px" }} />
+            </div>
+            <div className="d-flex justify-content-evenly text-center mt-3" >
+                <p className="text-center" style={{ width: "21%" }}>From Paralyzed to Pampered: The Inspirational Journey of a Street Dog in the USA In the heartwarming world of animal rescue,[ ... ]</p>
+                <p style={{ width: "21%" }}>From Indian Streets to Texas Rodeo Star: Barfi's Inspiring Journey to a Forever Home Everyone loves a heartwarming tale of[ ... ]</p>
+                <p style={{ width: "21%", fontSize: "15px" }}>On the streets of India, dogs are hated like the plague. This discrimination makes people believe that street dogs are non- adoptable . Through[ ... ]</p>
+
+            </div>
+        </div>
+        <div className="Container-fluid mt-4 d-flex justify-content-center align-items-center" style={{ height: "400px", backgroundColor: "rgb(246,124,35)" }}>
+
+            <div style={{ height: "370px", width: "50%", backgroundColor: "black", color: "white" }} >
+                <video width="550" height="340" controls autoplay loop>
+                    <source src="https://youtube.com/shorts/v-EK3YvmfV4?si=GwWfiXhJqwHICHmB" type="video/mp4" />
+                </video>
+            </div>
+
+        </div>
+    </div>
+}
+export default Home;
