9fdfa043eb01d78b56d54bb042f01123141f4e84
Shyamtawar02 <140075491+Shyamtawar02@users.noreply.github.com>
2024-04-24 16:59:24 +0530

    Update Footer.js

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -2,38 +2,40 @@ function Footer(){
     return<>
          <div className="container-fluid mt-3">
             <div className="container d-flex justify-content-center ">
-                <img src="https://i0.wp.com/strayanimalfoundationindia.org/wp-content/uploads/2022/04/DogHead.png?resize=800%2C314&ssl=1" />
+                <img className="img-fluid" src="https://i0.wp.com/strayanimalfoundationindia.org/wp-content/uploads/2022/04/DogHead.png?resize=800%2C314&ssl=1" />
             </div>
-            <div className="container-fluid rounded-4 d-flex justify-content-evenly" style={{ height: "260px", backgroundColor: "rgb(87,201,130)" }} >
-                <div className=" d-flex-column justify-content-center mt-5 text-center text-light ">
-                    <h3 style={{ fontWeight: "bold" }}>For India inquiries :</h3>
+            <div className="container-fluid rounded-4 d-flex justify-content-evenly" style={{ backgroundColor: "rgb(87,201,130)" }} >
+            <div className="row row-cols-lg-4 row-cols-md-2 row-cols-sm-1 row-cols-1 w-100">
+                <div className=" col d-flex-column justify-content-center mt-5 text-center text-light ">
+                    <h3 style={{ fontWeight:"bold" }}>For India inquiries :</h3>
                     <p style={{ fontWeight: "bold" }}>animalscare1@gmail.com</p>
                     <p style={{ fontWeight: "bold" }}>71,Vijay Shree Nagar,Indore Airport Road Indore</p>
                     <p style={{ fontWeight: "bold" }}>9876543210,7894561230</p>
                 </div>
-                <div className="d-flex-column mt-5 text-center text-light">
-                    <h3 style={{ fontWeight: "bold" }}>Way To help</h3>
+                <div className=" col d-flex-column mt-5  text-center text-light">
+                    <h3 style={{ fontWeight:"bold"}}>Way To help</h3>
                     <p style={{ fontWeight: "bold" }}>Donation Options</p>
                     <p style={{ fontWeight: "bold" }}>Adopt</p>
                     <p style={{ fontWeight: "bold" }}>Report Animal Abuse or Cruelty</p>
                 </div>
-                <div className="d-flex-column mt-5 text-center text-light">
-                    <h3 style={{ fontWeight: "bold" }}>About Us</h3>
+                <div className=" col d-flex-column mt-5  text-center text-light">
+                    <h3 style={{ fontWeight:"bold" }}>About Us</h3>
                     <p style={{ fontWeight: "bold" }}>Staff</p>
                     <p style={{ fontWeight: "bold" }}>Gurantees & Partners</p>
                     <p style={{ fontWeight: "bold" }}>Press & Mentions</p>
                 </div>
-                <div className="d-flex-column mt-5 text-center text-light">
+                <div className=" col d-flex-column mt-5  text-center text-light">
                     <h3 style={{ fontWeight: "bold" }}>What We Do</h3>
                     <p style={{ fontWeight: "bold" }}>Adoptions</p>
                     <p style={{ fontWeight: "bold" }}>Release</p>
                     <p style={{ fontWeight: "bold" }}>FAQ*</p>
                 </div>
 
-
+        </div>
             </div>
 
         </div>
+
 
         </>
 }
