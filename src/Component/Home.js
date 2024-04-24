6dc6e54cf76ab6023e563243dc35e93f57859cc3
Shyamtawar02 <140075491+Shyamtawar02@users.noreply.github.com>
2024-04-24 17:39:33 +0530

    Update Home.js

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -2,6 +2,9 @@ import img1 from "../Assets/img1.jpg";
 import img2 from "../Assets/img4.jpeg";
 import img3 from "../Assets/Volunteer General-save2_0.jpeg";
 import multi from "../Assets/multi.png";
+import Blocks from "../Component/Blocks";
+import Blocks2 from "../Component/Blocks2"
+
 function Home() {
     return <div className="w-100" style={{ height: "auto" }}>
 
@@ -85,101 +88,9 @@ function Home() {
 
             </div>
         </div>
-        <div className="conatiner-fluid mt-3  w-100" style={{ backgroundColor: "rgb(108,229,154)" }}>
-            <div className="row row-cols-lg-1 row-cols-md-1 row-cols-sm-1 row-cols-1 w-100">
-                <div className="col d-flex justify-content-center text-light">
-                    <h4 className="mt-3">AT A GLANCE</h4>
-                </div>
-            </div>
+            <Blocks/>
 
-
-            <div className=" d-flex mt-5 justify-content-evenly text-light  row row-cols-lg-3 row-cols-md-2 row-cols-sm-1 row-cols-1 w-100">
-                <div className=" col mb-5 border border-light rounded-4" style={{ height: "230px", width: "190px", backgroundColor: "rgb(87,201,130)" }}>
-                    <div className="d-flex justify-content-center">
-                        <img alt="" src="https://i0.wp.com/strayanimalfoundationindia.org/wp-content/uploads/2022/04/SAFI_1.png?resize=601%2C601&ssl=1" style={{ height: "120px", width: "100px", marginTop: "13px" }} />
-                    </div>
-                    <div className="d-flex justify-content-center">
-                        <h4>1500+</h4>
-                    </div>
-                    <div className="d-flex justify-content-center">
-                        <p style={{ fontSize: "14px" }}>saved animal</p>
-                    </div>
-                </div>
-                <div className=" col mb-5 border border-light rounded-4" style={{ height: "230px", width: "190px", backgroundColor: "rgb(87,201,130)" }}>
-                    <div className="d-flex justify-content-center">
-                        <img className="img-fluid" alt="" src="https://i0.wp.com/strayanimalfoundationindia.org/wp-content/uploads/2022/04/SAFI_2.png?resize=601%2C601&ssl=1" style={{ height: "120px", width: "100px", marginTop: "13px" }} />
-                    </div>
-                    <div className="d-flex justify-content-center">
-                        <h4>950+</h4>
-                    </div>
-                    <div className="d-flex justify-content-center text-center">
-                        <p style={{ fontSize: "14px", width: "60%" }}>animals spayed & neutered</p>
-                    </div>
-                </div>
-                <div className=" col mb-5 border border-light rounded-4" style={{ height: "230px", width: "190px", backgroundColor: "rgb(87,201,130)" }}>
-                    <div className="d-flex justify-content-center">
-                        <img alt="" src="https://i0.wp.com/strayanimalfoundationindia.org/wp-content/uploads/2022/04/SAFI_5.png?resize=601%2C601&ssl=1" style={{ height: "120px", width: "100px", marginTop: "13px" }} />
-                    </div>
-                    <div className="d-flex justify-content-center">
-                        <h4>120+</h4>
-                    </div>
-                    <div className="d-flex justify-content-center text-center">
-                        <p style={{ fontSize: "14px", width: "60%" }}>dogs adopted, 310 released</p>
-                    </div>
-                </div>
-            </div>
-
-
-        </div>
-
-        <div className="container-fluid ">
-            <div className=" mt-3  d-flex justify-content-evenly align-items-center row row-cols-lg-4 row-cols-md-2 row-cols-sm-1 row-cols-1 w-100">
-                <div className=" col rounded-2 mt-3 " style={{ height: "180px", width: "220px", backgroundColor: "rgb(87,201,130)" }}>
-                    <div className="d-flex justify-content-center text-light mt-3 ">
-                        <h5>DONATE</h5>
-                    </div>
-                    <div className="d-flex justify-content-center text-center text-light">
-                        <p className="text-center" style={{ fontSize: "13px", width: "60%" }}>Every rupees makes difference  the lives  these animals. We offers many ways</p>
-                    </div>
-                    <div className="d-flex justify-content-center ">
-                        <button type="button" style={{ fontSize: "10px", color: "white", backgroundColor: "rgb(246,124,35)", border: "none" }}>Learn More</button>
-                    </div>
-                </div>
-                <div className="col rounded-2 mt-3" style={{ height: "180px", width: "220px", backgroundColor: "rgb(246,124,35)" }}>
-                    <div className="d-flex justify-content-center text-light mt-3 ">
-                        <h5>ADOPT</h5>
-                    </div>
-                    <div className="d-flex justify-content-center text-center text-light">
-                        <p className="text-center" style={{ fontSize: "13px", width: "62%" }}>Everyone deserves a home. See how you can help our shelter animals find  families</p>
-                    </div>
-                    <div className="d-flex justify-content-center ">
-                        <button type="button" style={{ fontSize: "10px", color: "white", backgroundColor: "rgb(87,201,130)", border: "none" }}>Learn More</button>
-                    </div>
-                </div>
-                <div className=" col rounded-2 mt-3" style={{ height: "180px", width: "220px", backgroundColor: "rgb(87,201,130)" }}>
-                    <div className="d-flex justify-content-center text-light mt-3 ">
-                        <h5>Rescue Animals</h5>
-                    </div>
-                    <div className="d-flex justify-content-center align-items-center text-center text-light ">
-                        <p class="text-center" style={{ fontSize: "13px", width: "68%" }}>Every creature deserves compassion and care, animals need of a loving home.</p>
-                    </div>
-                    <div className="d-flex justify-content-center ">
-                        <button type="button" style={{ fontSize: "10px", color: "white", backgroundColor: "rgb(246,124,35)", border: "none" }}>Learn More</button>
-                    </div>
-                </div>
-                <div className=" col rounded-2 mt-3" style={{ height: "180px", width: "220px", backgroundColor: "rgb(246,124,35)" }}>
-                    <div className="d-flex justify-content-center text-light mt-3 ">
-                        <h5>Doctors</h5>
-                    </div>
-                    <div className="d-flex justify-content-center align-items-center text-center text-light">
-                        <p className="text-center" style={{ fontSize: "13px", width: "65%" }}>Every life is precious, and our doctors work tirelessly to save and improve them.</p>
-                    </div>
-                    <div className="d-flex justify-content-center ">
-                        <button type="button" style={{ fontSize: "10px", color: "white", backgroundColor: "rgb(87,201,130)", border: "none" }}>Learn More</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+           <Blocks2/>
         <div className="container  border border-dark rounded-5 mt-3">
             <div className=" d-flex justify-content-evenly mt-5 row row-cols-lg-3 row-cols-md-2 row-cols-sm-1 row-cols-1 w-100" >
                 <div className="col">
